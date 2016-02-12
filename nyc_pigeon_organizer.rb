ca7dc11fa1c334539f2f761960893ad3a2e11271
jes708 <jonschwarz@nyu.edu>
2016-02-11 23:05:27 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new
+
+  data.each do |stat, subs|
+    data[stat].each do |sub, names|
+      data[stat][sub].each do |name|
+        new_hash[name] = {}
+      end
+    end
+  end
+
+  data.each do |stat, subs|
+    data[stat].each do |sub, names|
+      data[stat][sub].each do |name|
+        new_hash[name].merge!(stat => [])
+      end
+    end
+  end
+
+  data.each do |stat, subs|
+    data[stat].each do |sub, names|
+      data[stat][sub].each do |name|
+        new_hash[name][stat] << sub.to_s
+      end
+    end
+  end
+
+  new_hash
 end
